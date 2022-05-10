710d67c0e0b2bfdffc17bb97fc3f06493767be9c
Maria Paula Duque <92931247+Mariaduque22@users.noreply.github.com>
2022-05-10 10:08:00 -0500

    fabricante

--- src/main/java/Fabricante.java
+++ src/main/java/Fabricante.java
@@ -1,5 +1,45 @@
 package vehiculos;
 
-public class Fabricante {
+import java.util.Map.Entry;
+import java.util.Hashtable;
 
+
+public class Fabricante {
+	private String nombre;
+	private Pais pais;
+	static Hashtable<Fabricante, Integer> vehiculosXFabricante = new Hashtable<Fabricante, Integer>();
+	
+	public Fabricante(String nombre, Pais pais) {
+		this.nombre = nombre;
+		this.pais = pais;
+	}
+
+	
+	public static Fabricante fabricaMayorVentas() {
+		int mayor = 0;
+		Fabricante fabricaMayorVentas = null;
+		for (Entry<Fabricante, Integer> entry : vehiculosXFabricante.entrySet()) {
+			if (entry.getValue() > mayor) {
+				mayor= entry.getValue();
+				fabricaMayorVentas = entry.getKey();
+			}
+		}
+		return fabricaMayorVentas;
+	}
+	
+	public Pais getPais() {
+		return this.pais;
+	}
+	
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
 }
